6ccdeeb6290285eb24c06e8e81b7b2c957b57cff
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-26 11:29:07 +0600

    added simple login handler and prevent default

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// console.log(56464)
+
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault()
+
+    const phoneNumber =document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+
+    console.log(64654)
+})
