902bb1f8086e550cc25f55c9137b9cc4c9976298
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-27 01:16:57 +0600

    form add to money

--- js/home.js
+++ js/home.js
@@ -6,6 +6,15 @@ document.getElementById('btn-add-mony').addEventListener('click',function(event)
 
     const addInputMoney =document.getElementById('add-money-input').value;
     const addPinNumber =document.getElementById('add-pin-input').value;
+
+    if(addPinNumber === '1234'){
+        console.log('adding money to your account');
+        
+    }
+    else{
+        alert('Failed to add money! please try again')
+    }
+
     console.log(addInputMoney,addPinNumber)
 
 
