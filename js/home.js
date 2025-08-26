b17cc15e983641f8927a60e1a024d11b6211beaf
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-26 13:29:27 +0600

    form data amount data

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
 console.log(656565)
+
+document.getElementById('btn-add-mony').addEventListener('click',function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    const addInputMoney =document.getElementById('add-money-input').value;
+    const addPinNumber =document.getElementById('add-pin-input').value;
+    console.log(addInputMoney,addPinNumber)
+
+
+    // console.log(6465)
+
+})
