2ae1aa63ab60439baf549fb5391b326ece8563fa
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-27 02:18:57 +0600

    add money and button hidden

---
+++ js/features.js
@@ -0,0 +1,12 @@
+console.log(544545);
+
+// show ht cash out form
+
+document.getElementById('btn-show-cash-out').addEventListener('click',function(){
+    document.getElementById('cast-out-form').classList.remove('hidden')
+    document.getElementById('add-money-form').classList.add('hidden')
+})
+document.getElementById('btn-show-add-money').addEventListener('click',function(){
+    document.getElementById('cast-out-form').classList.add('hidden')
+    document.getElementById('add-money-form').classList.remove('hidden')
+})
